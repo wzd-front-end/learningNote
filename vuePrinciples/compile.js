61b0b0314d7afa143b42c55dc4c0e787a45a28cb
wuzd@mybilin.cn <wuzd@mybilin.cn>
2019-03-07 16:06:59 +0800

    新建compile

---
+++ vuePrinciples/compile.js
@@ -0,0 +1,196 @@
+function Compile(el, vm) {
+    this.$vm = vm;
+    this.$el = this.isElementNode(el) ? el : document.querySelector(el);
+
+    if (this.$el) {
+        this.$fragment = this.node2Fragment(this.$el);
+        this.init();
+        this.$el.appendChild(this.$fragment);
+    }
+}
+
+Compile.prototype = {
+    node2Fragment: function (el) {
+        var fragment = document.createDocumentFragment(), child;
+
+        while (child = el.firstChild) {
+            fragment.appendChild(child);
+        }
+        return fragment
+    },
+
+    init: function () {
+        this.compileElement(this.$fragment);
+    },
+
+    compileElement: function (el) {
+        var childNodes = el.childNodes, me = this;
+
+        //节点数组实际上是伪数组，需要将起转换为数组 才可以进行遍历
+        Array.prototype.slice.call(childNodes).forEach(function (node) {
+            var text = node.textContent;
+            var reg = /\{\{(.*)\}\}/
+
+            if (me.isElementNode(node)) {
+                me.compile(node);
+            } else if (me.isTextNode(node) && reg.test(text)) {
+                me.compileText(node, RegExp.$1);
+            }
+
+            if (node.childNodes && node.childNodes.length) {
+                me.compileElement(node);
+            }
+        })
+    },
+
+    compile: function (node) {
+        var nodeAttrs = node.attributes, me = this;
+
+
+        //属性数组是类数组，用原型的方法将其改为数组
+        [].slice.call(nodeAttrs).forEach(function (attr) {
+            var attrName = attr.name;
+            if (me.isDirective(attrName)) {
+                var exp = attr.value;
+                var dir = attrName.substring(2);
+
+                //判断是否是事件指令
+                if (me.isEventDirective(dir)) {
+                    compileUtil.eventHandler(node, me.$vm, exp, dir)
+                } else {
+                    compileUtil[dir] && compileUtil[dir](node, me.$vm, exp);
+                }
+
+                node.removeAttribute(attrName);
+            }
+        })
+    },
+
+    compileText: function (node, exp) {
+        compileUtil.text(node, this.$vm, exp)
+    },
+
+    //工具函数
+    isDirective: function (attr) {
+        return attr.indexOf('v-') === 0;
+    },
+
+    isEventDirective: function (dir) {
+        return dir.indexOf('on') === 0;
+    },
+
+    isElementNode: function (node) {
+        return node.nodeType == 1;
+    },
+    isTextNode: function (node) {
+        return node.nodeType == 3;
+    }
+}
+
+//指令处理集合
+var compileUtil = {
+    text: function (node, vm, exp) {
+        this.bind(node, vm, exp, 'text');
+    },
+
+    html: function (node, vm, exp) {
+        this.bind(node, vm, exp, 'html');
+    },
+
+    class: function(node, vm, exp){
+        this.bind(node, vm, exp, 'class');
+    },
+
+    bind: function (node, vm, exp, dir) {
+        var updaterFn = updater[dir + 'Updater'];
+
+        updaterFn && updaterFn(node, this._getVMVal(vm, exp));
+
+        new Watcher(vm, exp, function (value, oldValue) {
+            updaterFn && updaterFn(node, value, oldValue)
+        })
+    },
+
+    model: function(node, vm, exp){
+        var me = this, val = this._getVMVal(vm, exp);
+        me.bind(node, vm, exp, 'model');
+
+        node.addEventListener('input',function (e) {
+            var newValue = e.target.value;
+
+            if(val === newValue){
+                return;
+            }
+
+            me._setVMVal(vm, exp, newValue);
+            val = newValue;
+        });
+    },
+
+    eventHandler: function (node, vm, exp, dir) {
+        var eventType = dir.split(':')[1],
+            fn = vm.$options.methods && vm.$options.methods[exp];
+        if (eventType && fn) {
+            node.addEventListener(eventType, fn.bind(vm), false);
+        }
+    },
+
+    _getVMVal: function (vm, exp) {
+        var val = vm;
+        exp = exp.split(".");
+        exp.forEach(function (k) {
+            val = val[k];
+        });
+
+        return val;
+    },
+
+    _setVMVal: function (vm, exp, value) {
+        var val = vm;
+        exp = exp.split(".");
+        exp.forEach(function (k, i) {
+            if(i < exp.length - 1){
+                val=val[k];
+            }else {
+                val=value;
+            }
+        });
+    }
+};
+
+var updater = {
+    textUpdater: function (node, value) {
+        node.textContent = typeof value == 'undefined' ? '' : value;
+    },
+
+    htmlUpdater: function (node, value) {
+        node.innerHTML = typeof value == 'undefined' ? '' : value;
+    },
+    
+    classUpdater: function (node, value, oldValue) {
+        var className =node.className;
+        className = className.replace(oldValue,'').replace(/\s$/,'');
+
+        var space = className && String(value) ? ' ' : '';
+        node.className = className + space + value;
+    },
+
+    modelUpdater: function (node, value, oldValue) {
+        node.value = typeof value == 'undefined' ? '' : value;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
