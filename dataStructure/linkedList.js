f0f8dbb7bb7bb12ead0ac17c2fc1a3c4e82a0883
wuzd@mybilin.cn <wuzd@mybilin.cn>
2019-03-14 16:00:27 +0800

    双向链表的实现

--- dataStructure/linkedList.js
+++ dataStructure/linkedList.js
@@ -6,16 +6,19 @@
 function Node(element) {
     this.element = element;
     this.next =null;
+    this.previous = null;//修改完双向链表
 }
 
 //LinkedList类
 function LList() {
     this.head = new Node("head");
     this.find = find;
+    this.findLast = findLast;
     this.insert = insert;
     this.findPrevious = findPrevious;
     this.remove = remove;
     this.display = display;
+    this.dispReverse = dispReverse;
 
     function find(item) {
         var currNode = this.head;
@@ -32,6 +35,7 @@ function LList() {
         var  current = this.find(item);
 
         newNode.next = current.next;
+        newNode.previous = current;//双向链表元素前置值设置
         current.next = newNode;
     }
     
@@ -52,12 +56,35 @@ function LList() {
     }
 
     function remove(element) {
-        var prevNode = this.findPrevious(element);
-        if(!(prevNode.next == null)){
-            prevNode.next = prevNode.next.next;
+        // var prevNode = this.findPrevious(element);//该句用于解决单向链表删除值得方法
+        var currentNode = this.find(element);
+
+        if(!(currentNode == null)){
+            currentNode.previous.next = currentNode.next;
+            currentNode.next.previous = currentNode.previous;
+            currentNode.next = null;
+            currentNode.previous = null;
+        }
+        // if(!(prevNode.next == null)){
+        //     prevNode.next = prevNode.next.next;//该句用于解决单向链表删除值得方法
+        // }
+    }
+    
+    function findLast() {
+        var currNode =this.head;
+        while (!(currNode.next == null)){
+            currNode = currNode.next;
+        }
+        return currNode;
+    }
+    
+    function dispReverse() {
+        var currNode = this.findLast();
+        while (!(currNode.previous == null)){
+            console.log(currNode.element)
+            currNode = currNode.previous;
         }
     }
-
 
 }
 
@@ -72,6 +99,8 @@ cities.display();
 cities.remove("test");
 console.log("删除后链表");
 cities.display();
+console.log("链表反向打印");
+cities.dispReverse();
 
 
 
