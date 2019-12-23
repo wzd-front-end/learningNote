e15e96c4f857529f2658e515c7864f719bfbe6ce
wuzd <429023964@qq.com>
2019-12-23 10:21:04 +0800

    feat：理解动态规划

--- dataStructure/14-Senior.js
+++ dataStructure/14-Senior.js
@@ -106,5 +106,36 @@ function lcs(word1, word2) {
   }
 }
 
-console.log(lcs('测试公共字符串', '我测不测还不知道，现实是前半部分，现在可以测试公共字符串，测试完毕'))
+// console.log(lcs('测试公共字符串', '我测不测还不知道，现实是前半部分，现在可以测试公共字符串，测试完毕'))
+
+function max(a, b) {
+  return (a > b) ? a : b;
+}
+
+function dKnapsack(capacity, size, value, n) {
+  var K = [];
+  for (var i = 0; i <= n; i++) {
+    K[i] = [];
+  }
+  for (var i = 0; i <= n; i++) {
+    for (var w = 0; w <= capacity; w++) {
+      if (i == 0 || w == 0) {
+        K[i][w] = 0;
+      } else if (size[i - 1] <= w) {
+        K[i][w] = max(value[i - 1] + K[i - 1][w - size[i - 1]], K[i - 1][w]);
+      } else {
+        K[i][w] = K[i - 1][w];
+      }
+
+    }
+  }
+  console.log(K);
+  return K[n][capacity];
+}
+
+var value = [4, 5, 10, 11, 13];
+var size = [3, 4, 7, 8, 9];
+var capacity = 16;
+var n = 5;
+console.log(dKnapsack(capacity, size, value, n));
 
