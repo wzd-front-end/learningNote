698ad60444c4e7528c01e49cea239ac55a7650af
wuzd <429023964@qq.com>
2020-01-08 18:28:58 +0800

    fix：增加合并区间案例

--- dataStructure/6-LinkedList.js
+++ dataStructure/6-LinkedList.js
@@ -4,94 +4,94 @@
 
 //Node类
 function Node(element) {
-    this.element = element;
-    this.next =null;
-    this.previous = null;//修改完双向链表
+  this.element = element;
+  this.next = null;
+  this.previous = null;//修改完双向链表
 }
 
 //LinkedList类
 function LList() {
-    this.head = new Node("head");
-    this.find = find;
-    this.findLast = findLast;
-    this.insert = insert;
-    this.findPrevious = findPrevious;
-    this.remove = remove;
-    this.display = display;
-    this.dispReverse = dispReverse;
+  this.head = new Node("head");
+  this.find = find;
+  this.findLast = findLast;
+  this.insert = insert;
+  this.findPrevious = findPrevious;
+  this.remove = remove;
+  this.display = display;
+  this.dispReverse = dispReverse;
 
-    function find(item) {
-        var currNode = this.head;
+  function find(item) {
+    var currNode = this.head;
 
-        while (currNode.element != item){
-            currNode = currNode.next;
-        }
-
-        return currNode;
+    while (currNode.element != item) {
+      currNode = currNode.next;
     }
 
-    function insert(newElement, item) {
-        var newNode = new Node(newElement);
-        var current = this.find(item);
+    return currNode;
+  }
 
-        newNode.next = current.next;
-        newNode.previous = current;//双向链表元素前置值设置
-        current.next = newNode;
+  function insert(newElement, item) {
+    var newNode = new Node(newElement);
+    var current = this.find(item);
+
+    newNode.next = current.next;
+    newNode.previous = current;//双向链表元素前置值设置
+    current.next = newNode;
+  }
+
+  function display() {
+    var currNode = this.head;
+    while (!(currNode.next == null)) {
+      console.log(currNode.next.element);
+      currNode = currNode.next;
     }
+  }
 
-    function display() {
-        var currNode = this.head;
-        while (!(currNode.next == null)){
-            console.log(currNode.next.element);
-            currNode = currNode.next;
-        }
+  function findPrevious(item) {
+    var currNode = this.head;
+    while (!(currNode.next == null) && (currNode.next.element != item)) {
+      currNode = currNode.next;
     }
+    return currNode;
+  }
 
-    function findPrevious(item) {
-        var currNode = this.head;
-        while (!(currNode.next == null) && (currNode.next.element != item)){
-            currNode = currNode.next;
-        }
-        return currNode;
+  function remove(element) {
+    // var prevNode = this.findPrevious(element);//该句用于解决单向链表删除值得方法
+    var currentNode = this.find(element);
+
+    if (!(currentNode == null)) {
+      currentNode.previous.next = currentNode.next;
+      currentNode.next.previous = currentNode.previous;
+      currentNode.next = null;
+      currentNode.previous = null;
     }
+    // if(!(prevNode.next == null)){
+    //     prevNode.next = prevNode.next.next;//该句用于解决单向链表删除值得方法
+    // }
+  }
 
-    function remove(element) {
-        // var prevNode = this.findPrevious(element);//该句用于解决单向链表删除值得方法
-        var currentNode = this.find(element);
-
-        if(!(currentNode == null)){
-            currentNode.previous.next = currentNode.next;
-            currentNode.next.previous = currentNode.previous;
-            currentNode.next = null;
-            currentNode.previous = null;
-        }
-        // if(!(prevNode.next == null)){
-        //     prevNode.next = prevNode.next.next;//该句用于解决单向链表删除值得方法
-        // }
+  function findLast() {
+    var currNode = this.head;
+    while (!(currNode.next == null)) {
+      currNode = currNode.next;
     }
+    return currNode;
+  }
 
-    function findLast() {
-        var currNode =this.head;
-        while (!(currNode.next == null)){
-            currNode = currNode.next;
-        }
-        return currNode;
+  function dispReverse() {
+    var currNode = this.findLast();
+    while (!(currNode.previous == null)) {
+      console.log(currNode.element)
+      currNode = currNode.previous;
     }
-
-    function dispReverse() {
-        var currNode = this.findLast();
-        while (!(currNode.previous == null)){
-            console.log(currNode.element)
-            currNode = currNode.previous;
-        }
-    }
+  }
 
 }
 
 var cities = new LList();
 cities.insert("Conway", "head");
 cities.insert("Russellvielle", "Conway");
-cities.insert("test","Russellvielle")
+cities.insert("test", "Russellvielle")
 cities.insert("Alma", "test");
 
 console.log("删除前链表")
---
+++ dataStructure/mergingInterval.js
@@ -0,0 +1,10 @@
+/**
+ * 合并区间
+ *[mergeInterval 合并区间]
+ * @param  {[array]} interval [需要合并的区间，该值为一个二维数组]
+ * @return {[array]}      [合并后的结果，该值为一个二维数组]
+ *
+ */
+function f() {
+
+}
